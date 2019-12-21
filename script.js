14740f0259e0b0b8fef806769dc04b384b1c5a09
Ben Hernandez <benjammin234@gmail.com>
2019-12-21 09:28:28 -0800

    created fn watchForm

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchHandle = $('#js-github-handle').val();
+        getRepos(searchHandle);
+    });
+}
+
+
+
+
+$(`https://api.github.com/users/${user-handle}/repos`)
+
+
+
+
+
+
+
+$(watchForm);
